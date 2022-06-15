1f05c940eda0b94a7a90534fbaa82a28cc757b09
Emily Hsueh <hsue0002@algonquinlive.com>
2022-06-15 15:05:42 -0400

    Adds for loop

--- mtm6302-exercise-1/script.js
+++ mtm6302-exercise-1/script.js
@@ -5,7 +5,7 @@ const emojis = []
 for (let i = 129410; i < 129511; i++) {
   emojis.push(`
     <div class="emoji">&#${i}</div>
-    <code class="code">${i}</code> 
+    <div class="code">${i}</div> 
     `)
 }
 
